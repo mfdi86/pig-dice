1846426574390bb2b6199cbfa19c46e6eccf8c96
mdc <maria.f.delcastillo@gmail.com>
2017-05-10 15:44:30 -0700

    Add HTML 'Roll' and 'Hold' and initial JS functionality

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+//Back-End Logic:
+
+function Player(name, score) {
+  this.name = name;
+  this.score = score;
+}
+
+Player.prototype.tallyScore = function() {
+    var tally = 0;
+  if (this.score !== 1, 0) {
+    //add number to "your score"
+  }
+}
+
+var playerA = new Player("player A", 0);
+var playerB = new Player("player B", 0);
+
+// while neither player's score is above 100pts
+
+// Player A turn
+// roll a die
+// if (roll === 1 ){
+// turn over, no points
+// }
+// else {
+//  add currentRollPoints to currentRoundScore
+//
+// function Roll ()
+//    this.previous
+//    this.current
+// Roll.prototype.currentScore = function() {
+//    return this.score + previous score
+
+  // if user wants to end turn {
+  // add currentRoundScore to totalScore
+    // }
+    // if user continues roll then ... go back to the top
+//}
+//Front-End Logic:
+
+$(document).ready(function() {
+  $("form#rollDice").submit(function(event) {
+    event.preventDefault();
+
+
+  var result = Math.floor(Math.random() * 7);
+    console.log(result);
+    $("#output").append(result);
+    });
+
+  $("form#hold").submit(function(event) {
+    event.preventDefault();
+
+
+
+//  var result = "0";
+//  $("#score").append(result);
+  });
+});
