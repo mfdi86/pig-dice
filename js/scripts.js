b09c4400da47a397166ea3b693af9ca2a210353b
mdc <maria.f.delcastillo@gmail.com>
2017-05-11 16:25:16 -0700

    Add JS that sets score to 0 if 1 is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
 //Back-End Logic:
 
-function Player(name, score) {
+function Player(name, score, totalScore) {
   this.name = name;
-  this.score = score;
+  this.score = 0;
+  this.totalScore = 0;
 }
 
-Player.prototype.tallyScore = function() {
-    var tally = 0;
-  if (this.score !== 1, 0) {
-    //add number to "your-score" (with append or +)
+Player.prototype.tallyScore = function(player1) {
+  var tally = ((Math.floor(Math.random() * 6) +1));
+  console.log("TALLY = " + tally);
+  if (tally !== 1) {
+    player1.score += tally;
   }
-}
+  else {
+   tally = 0
+  }
 
-var playerA = new Player("player A", 0);
-var playerB = new Player("player B", 0);
+  console.log("Tally number 2 = " + tally);
+  return player1;
+  // add rolled number to "your-score", turn continues
+  }
+//     //turn is over, score tally = 0
+//   }
+// }
+
 
 // while neither player's score is above 100pts
-
-// Player A turn
-// roll a die
-// if (roll === 1 ){
-// turn over, no points added, subtract points in tally
-// }
-// else {
-//  add currentRollPoints to currentRoundScore
-//
 // function Roll ()
 //    this.previous
 //    this.current
@@ -39,21 +40,46 @@ var playerB = new Player("player B", 0);
 //Front-End Logic:
 
 $(document).ready(function() {
-  $("form#rollDice").submit(function(event) {
+  var player1 = new Player("player1", 0, 0);
+  var player2 = new Player("player2", 0, 0);
+
+
+  $("form#rollDice1").submit(function(event){
     event.preventDefault();
 
+    player1 = player1.tallyScore(player1);
+    // console.log(tallyScore);
 
-  var result = Math.floor(Math.random() * 6 +1);
-    console.log(result);
-    $("#output").text(result);
+    // var result1 = Math.floor(Math.random() * 6 +1);
+    console.log(player1.score);
+    $("#output1").text(player1.score);
+
+    $("#hold1").click(function(player1){
+      player1.totalScore = player1.totalScore + player1.score;
+      console.log(player1.score);
+      console.log(player1.totalScore);
+      player1.score = 0;
+      return player1;
     });
-
-  $("form#hold").submit(function(event) {
-    event.preventDefault();
-
-
-
-//  var result = "0";
-//  $("#score").append(result);
   });
 });
+
+//   $("form#hold1").submit(function(event) {
+//
+// //----------------------------------------------------
+//
+//   $("form#rollDice2").submit(function(event) {
+//
+//
+//   var result2 = Math.floor(Math.random() * 6 +1);
+//     console.log(result2);
+//     $("#output2").text(result2);
+//     });
+//
+//   $("form#hold2").submit(function(event) {
+//
+//
+// //  var result = "0";
+// //  $("#score").append(result);  });
+// });
+// });
